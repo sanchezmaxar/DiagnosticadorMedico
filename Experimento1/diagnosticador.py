0c230bf1c5af67bea610d870753e728dff342484
max <slemuh.ms@gmail.com>
2018-11-29 00:46:22 -0600

    actualizado

--- Experimento1/diagnosticador.py
+++ Experimento1/diagnosticador.py
@@ -13,12 +13,14 @@ def vectorPromedioDeTexto(words, model, num_features):
 	# Funcion modificada de https://datascience.stackexchange.com/questions/23969/sentence-similarity-prediction
 	featureVec = np.zeros((num_features,), dtype="float32")
 	nwords=0
+	global contador
 	for word in words:
 		try:
 			nwords = nwords+1
 			featureVec = np.add(featureVec, model[word])
 		except:
-			pass #ignoramos la palabra que conocemos
+			pass #ignoramos la palabra que no conocemos
+			contador+=1
 	if nwords>0:
 		featureVec = np.divide(featureVec, nwords)
 	return featureVec
@@ -26,7 +28,7 @@ def vectorPromedioDeTexto(words, model, num_features):
 def predecirSimilaridad(vector1,vector2):
 	return 1 - spatial.distance.cosine(vector1,vector2)
 
-
+contador=0
 wordvectors_file_vec = 'fasttext-sbwc.3.6.e20.vec.gz'
 cantidad = 100000
 num_features=300
@@ -37,7 +39,7 @@ corpus={}
 with open('datos.csv',"r",newline='') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 	for row in spamreader:
-		corpus[row[0]]=vectorPromedioDeTexto(textoALista(row[1]),wordvectors,num_features)
+		corpus[row[0]]=vectorPromedioDeTexto(textoALista(row[2]),wordvectors,num_features)
 		# print(corpus[row[0]])
 
 
@@ -47,5 +49,5 @@ with open(sys.argv[1],"r") as f:
 vectorDeSimilaridades={}
 for nombre,enfermedad in corpus.items():
 	vectorDeSimilaridades[nombre]=predecirSimilaridad(enfermedad,sintomas)
-
-printD(vectorDeSimilaridades)
+print(contador)
+# printD(vectorDeSimilaridades)
